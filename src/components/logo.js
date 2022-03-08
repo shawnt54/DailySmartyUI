591582787d7cf9b7a3472b82698923ebc005f161
shawnt54 <shawndthurlow@gmail.com>
2022-03-08 09:43:50 -0700

    mapped state to props in results posts component and rendered each post with our post component, and implemented home link in logo component

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Logo extends Component {
     render() {
@@ -10,7 +11,9 @@ class Logo extends Component {
 
         return (
             <div className="logo-main">
-                <img style={size} alt="daily smarty ui image logo big" src="/assets/ds_circle_logo.png"/>
+                <Link to="/">
+                    <img style={size} alt="daily smarty ui image logo big" src="/assets/ds_circle_logo.png"/>
+                </Link>
             </div>
         )
     }
